eb215fd3e9e3ead40bfbb242567e963cf7e958b6
Thysin <skthysinparveen29@gmail.com>
2023-10-18 16:14:25 +0530

    add package

---
+++ gitsample/src/test/java/Automationgit/Playsong_on_youtube.java
@@ -0,0 +1,14 @@
+package Automationgit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Playsong_on_youtube {
+
+	public static void main(String[] args)
+	{
+      WebDriver driver = new FirefoxDriver();
+      driver.get("https://www.youtube.com/");
+	}
+
+}
